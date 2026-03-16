27e06410523865c03eecc551e76253419961433a
maximealbert <114584696+maximealbert@users.noreply.github.com>
2026-03-16 09:43:42 +0100

    [formation] initial setup

---
+++ StickerSmash/components/EmojiSticker.tsx
@@ -0,0 +1,39 @@
+import { ImageSourcePropType, View } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+
+type Props = {
+    imageSize: number;
+    stickerSource: ImageSourcePropType;
+}
+
+export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+    const scaleImage = useSharedValue(imageSize)
+
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value)
+        }
+    })
+
+    const doubleTap = Gesture.Tap().numberOfTaps(2).onStart(() => {
+        if (scaleImage.value !== imageSize * 2) {
+            scaleImage.value = scaleImage.value * 2;
+        } else {
+            scaleImage.value = Math.round(scaleImage.value / 2)
+        }
+    })
+
+    return (
+        <View style={{ top: -350 }}>
+            <GestureDetector gesture={doubleTap}>
+                <Animated.Image
+                    source={stickerSource}
+                    resizeMode="contain"
+                    style={[imageStyle, { width: imageSize, height: imageSize }]}
+                />
+            </GestureDetector>
+        </View>
+    );
+}
